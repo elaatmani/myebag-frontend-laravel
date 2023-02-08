e7f235445f163bf9e9b23cdcc28870889926f339
ya9 <devyassine47@gmail.com>
2023-02-08 15:15:36 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <HelloWorld />
+  <div>Hello World</div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
 });
 </script>
