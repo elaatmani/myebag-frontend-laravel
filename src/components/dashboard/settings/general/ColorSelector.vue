d3ba412975e890f73fc5e6224960cc37a04b3e19
ya9 <devyassine47@gmail.com>
2023-05-17 13:01:17 +0100

    added colors

--- src/components/dashboard/settings/general/ColorSelector.vue
+++ src/components/dashboard/settings/general/ColorSelector.vue
@@ -10,9 +10,9 @@
         <div class="tw-flex tw-gap-2 tw-flex-wrap tw-mt-2">
           <div v-for="c in Object.keys(colors)" :key="c">
             <div
-              @click="handleClickPrimary(c)"
+              @click="handleClickPrimary({ name: c, light: colors[c][500], dark: colors[c][400] })"
               :class="[
-                c == primary && 'tw-ring dark:tw-ring-white tw-ring-black',
+                c == primary.name && 'tw-ring dark:tw-ring-white tw-ring-black',
               ]"
               :style="{ background: colors[c][500] }"
               class="tw-w-[30px] tw-h-[30px] tw-rounded tw-border tw-border-neutral-700/50 dark:tw-border-neutral-300/50 tw-cursor-pointer"
@@ -26,9 +26,9 @@
         <div class="tw-flex tw-gap-2 tw-flex-wrap tw-mt-2">
           <div v-for="c in Object.keys(colors)" :key="c">
             <div
-              @click="handleClickSecondary(c)"
+              @click="handleClickSecondary({name: c, light: colors[c][500], dark: colors[c][400] })"
               :class="[
-                c == secondary && 'tw-ring dark:tw-ring-white tw-ring-black',
+                c == secondary.name && 'tw-ring dark:tw-ring-white tw-ring-black',
               ]"
               :style="{ background: colors[c][500] }"
               class="tw-w-[30px] tw-h-[30px] tw-rounded tw-border tw-border-neutral-700/50 dark:tw-border-neutral-300/50 tw-cursor-pointer"
@@ -46,8 +46,16 @@ import colors from "tailwindcss/colors";
 export default {
   data() {
     return {
-      primary: "violet",
-      secondary: "emerald",
+      primary: {
+          "name": "violet",
+          "light": "#8b5cf6",
+          "dark": "#a78bfa"
+      },
+      secondary: {
+          "name": "emerald",
+          "light": "#10b981",
+          "dark": "#34d399"
+      },
     };
   },
 
@@ -80,19 +88,14 @@ export default {
     handleClickPrimary(c) {
       this.primary = c;
       this.$store.dispatch('app/setPrimary', c)
-      // console.log(this.$primary.value);
+      console.log(this.$primary.value);
     },
 
     handleClickSecondary(c) {
       this.secondary = c
       this.$store.dispatch('app/setSecondary', c)
-      // console.log(this.$secondary);
+      console.log(this.$secondary.value);
     },
-
-    getColorClasses(color) {
-      // Modify this function to return classes based on your requirements
-      return `tw-border tw-p-1 tw-rounded tw-bg-${color}-500/10 tw-border-${color}-500 tw-text-${color}-500`;
-    }
   },
 
   mounted() {
--- src/plugins/globalConfig.js
+++ src/plugins/globalConfig.js
@@ -9,5 +9,13 @@ export default {
 
         app.config.globalProperties.$primary = computed(() => app.config.globalProperties.$store.getters['app/primary'])
         app.config.globalProperties.$secondary = computed(() =>app.config.globalProperties.$store.getters['app/secondary'])
+        
+        // app.config.globalProperties.$getPrimaryClasses = (type = 'bg', shade = 500) => {
+        //     return `tw-${type}-${app.config.globalProperties.$primary.value}-${shade}`
+        // }
+
+        // app.config.globalProperties.$getSecondaryClasses = (type = 'bg', shade = 500) => {
+        //     return `tw-${type}-${app.config.globalProperties.$secondary.value}-${shade}`
+        // }
     }
 }
